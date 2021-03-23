236950d6c43eadc9f3033814b91dd4df9d6e52d7
Nektarios <48967921+PNektarios@users.noreply.github.com>
2021-03-23 19:26:11 +0200

    Using math library to calculate simple equations and return their result to the function

--- src/utils/checkNumCounting.js
+++ src/utils/checkNumCounting.js
@@ -1,13 +1,15 @@
-const { Client } = require('../index.js');
+const { Client } = require('../index.js'),
+    math = require('mathjs'),
+    { Error } = require('./Error.js');
 
-function addNew(message) {
+function addNew(message, number) {
 
-    if (!Client.pastNumbers.has(message.guild.id) && parseFloat(message.content) !== 1.0) {
-        message.channel.send(`Start from 1...`);
+    if (!Client.pastNumbers.has(message.guild.id) && parseFloat(number) !== 1.0) {
+        Error(`Wrong number`, `Start from 1...`, message);
         return;
     }
 
-    Client.pastNumbers.set(message.guild.id, [parseFloat(message.content), message.author.id]);
+    Client.pastNumbers.set(message.guild.id, [parseFloat(number), message.author.id]);
     message.react('🆗').then(r => {
         return;
     });
@@ -16,18 +18,26 @@ function addNew(message) {
 
 function checkNumber(message) {
 
+    let number = doMath(message.content);
+
     if (!Client.pastNumbers.get(message.guild.id)) {
-        return addNew(message);
+        return addNew(message, number);
     }
     if (Client.pastNumbers.get(message.guild.id)[1] === message.author.id) {
-        return message.channel.send(`I honestly get it if you dont have any friends but you cant play on your own.`)
-    } else if (Client.pastNumbers.get(message.guild.id)[0] === parseFloat(message.content) -1) {
-        return addNew(message);
+        return Error(`Two messages in a row`, `I honestly get it if you dont have any friends but you cant play on your own.`, message)
+    } else if (Client.pastNumbers.get(message.guild.id)[0] === parseFloat(number) -1) {
+        return addNew(message, number);
     } else {
-        message.channel.send(`${message.author.username} learn how to count bro...`);
+        Error(`Wrong number`, `${message.author.username} learn how to count bro...\nYou just ruined <@${Client.pastNumbers.get(message.guild.id)[1]}>' high score of ${Client.pastNumbers.get(message.guild.id)[0]}.`, message);
         Client.pastNumbers.delete(message.guild.id);
         return;
     }
+
+}
+
+function doMath(equation) {
+
+    return math.evaluate(equation);
 
 }
 
